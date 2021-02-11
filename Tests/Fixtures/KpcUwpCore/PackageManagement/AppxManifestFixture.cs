dc38557b8bfbe0d23a49439d09841501cb8d5b6e
radujipa <radu.t.jipa@gmail.com>
2021-02-11 14:03:15 +0000

    AppxManifest class for managing Package.appxmanifest files

---
+++ Tests/Fixtures/KpcUwpCore/PackageManagement/AppxManifestFixture.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * AppxManifestFixture.cs
+ *
+ * Copyright (c) 2021 Kano Computing Ltd.
+ * License: https://opensource.org/licenses/MIT
+ */
+
+
+using KanoComputing.KpcUwpCore.PackageManagement;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace KanoComputing.KpcUwpCore.Tests.Fixtures.KpcUwpCore.PackageManagement {
+
+    public class AppxManifestFixture {
+
+        public static readonly Uri AppxManifestPath =
+            new Uri("ms-appx:///Fixtures/KpcUwpCore/PackageManagement/Package.appxmanifest");
+
+        public static readonly IDictionary<string, string> Namespaces =
+            new Dictionary<string, string> {
+                { "xml", "http://www.w3.org/XML/1998/namespace" },
+                { "appx", "http://schemas.microsoft.com/appx/manifest/foundation/windows10" },
+                { "mp", "http://schemas.microsoft.com/appx/2014/phone/manifest" },
+                { "uap", "http://schemas.microsoft.com/appx/manifest/uap/windows10" },
+                { "uap3", "http://schemas.microsoft.com/appx/manifest/uap/windows10/3" },
+                { "uap4", "http://schemas.microsoft.com/appx/manifest/uap/windows10/4" },
+                { "desktop4", "http://schemas.microsoft.com/appx/manifest/desktop/windows10/4" },
+                { "iot2", "http://schemas.microsoft.com/appx/manifest/iot/windows10/2" },
+                { "rescap", "http://schemas.microsoft.com/appx/manifest/foundation/windows10/restrictedcapabilities" }
+            };
+
+        public static async Task<AppxManifest> GetAppxManifestAsync() {
+            AppxManifest manifest = new AppxManifest();
+            await manifest.LoadAsync(AppxManifestPath);
+            return manifest;
+        }
+    }
+}
---
+++ Tests/Unit/KpcUwpCore/PackageManagement/TestAppxManifest.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * TestAppxManifest.cs
+ *
+ * Copyright (c) 2021 Kano Computing Ltd.
+ * License: https://opensource.org/licenses/MIT
+ */
+
+
+using KanoComputing.KpcUwpCore.PackageManagement;
+using KanoComputing.KpcUwpCore.Tests.Fixtures.KpcUwpCore.PackageManagement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace KanoComputing.KpcUwpCore.Tests.Unit.KpcUwpCore.PackageManagement {
+
+    [TestClass]
+    public class TestAppxManifest {
+
+        [TestMethod]
+        public async Task TestLoadAsync() {
+            AppxManifest manifest = null;
+            try {
+                manifest = await AppxManifestFixture.GetAppxManifestAsync();
+            } catch (Exception e) {
+                Assert.Fail($"Caught: {e}");
+            }
+            Assert.IsNotNull(
+                manifest,
+                "There was an error loading the manifest file.");
+            Assert.IsNotNull(
+                manifest.Xml,
+                "The manifest XML document was not loaded properly.");
+            Assert.IsNotNull(
+                manifest.Xmlns,
+                "The manifest XML document namespaces were not loaded properly.");
+        }
+
+        [TestMethod]
+        public async Task TestXmlns() {
+            AppxManifest manifest = await AppxManifestFixture.GetAppxManifestAsync();
+            IDictionary<string, string> actualNamespaces =
+                manifest.Xmlns.GetNamespacesInScope(XmlNamespaceScope.All);
+
+            CollectionAssert.AreEqual(
+               AppxManifestFixture.Namespaces.OrderBy(kv => kv.Key).ToList(),
+               actualNamespaces.OrderBy(kv => kv.Key).ToList(),
+               "The class did not load all expected namespaces."
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("/appx:Package/appx:Applications/appx:Application[@Id='App']/appx:Extensions/uap:Extension[@Category='windows.protocol']/uap:Protocol")]
+        [DataRow("/appx:Package/appx:Resources/appx:Resource[@Language='EN-US']")]
+        [DataRow("/appx:Package/appx:Applications/appx:Application[@Id='App']/appx:Extensions/appx:Extension[@Category='windows.preInstalledConfigTask']")]
+        [DataRow("/appx:Package/appx:Capabilities/rescap:Capability")]
+        [DataRow("/appx:Package/appx:Applications/appx:Application[@Id='App']/uap:ApplicationContentUriRules/uap:Rule")]
+        public async Task TestXmlQueries(string xpath) {
+            AppxManifest manifest = await AppxManifestFixture.GetAppxManifestAsync();
+            XmlNode node = manifest.Xml.DocumentElement.SelectSingleNode(xpath, manifest.Xmlns);
+
+            Assert.IsNotNull(
+                node,
+                "Could select a node from the manifest. Is it loaded properly?");
+        }
+    }
+}
