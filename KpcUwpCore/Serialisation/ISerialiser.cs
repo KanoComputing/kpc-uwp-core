56c7bb6b949ef68e083575c234e265771cd052bb
radujipa <radu.t.jipa@gmail.com>
2021-05-18 19:56:11 +0100

    JSON serialiser for DataContract objects

---
+++ KpcUwpCore/Serialisation/ISerialiser.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * ISerialiser.cs
+ *
+ * Copyright (c) 2021 Kano Computing Ltd.
+ * License: https://opensource.org/licenses/MIT
+ */
+
+
+namespace KanoComputing.KpcUwpCore.Serialisation {
+
+    public interface ISerialiser {
+
+        Contract DeserialiseJsonToDataContract<Contract>(string serialisedData);
+        string SerialiseDataContractToJson<Contract>(object obj);
+    }
+}
