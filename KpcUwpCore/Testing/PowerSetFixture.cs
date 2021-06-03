65c26de5e586445b5cce9e9eb8df40b02d8b8422
radujipa <radu.t.jipa@gmail.com>
2021-06-03 15:49:50 +0100

    Testing fixtures and helpers

---
+++ KpcUwpCore/Testing/PowerSetFixture.cs
@@ -0,0 +1,60 @@
+﻿/**
+ * PowerSetFixture.cs
+ *
+ * Copyright (c) 2021 Kano Computing Ltd.
+ * License: https://opensource.org/licenses/MIT
+ */
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+
+namespace KanoComputing.KpcUwpCore.Testing {
+
+    public class PowerSetFixture : Attribute, ITestDataSource {
+
+        private readonly ITestDataSource fixture;
+        private readonly Type dataType;
+
+        public PowerSetFixture(Type fixture, Type dataType) {
+            this.fixture = Activator.CreateInstance(fixture) as ITestDataSource;
+            this.dataType = dataType;
+        }
+
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo) {
+            IEnumerable<IEnumerable<object>> powerSet = TestHelpers.GetPowerSet<object>(
+                this.fixture.GetData(methodInfo).Select(x => x[0])
+            );
+
+            foreach (IEnumerable<object> objs in powerSet) {
+                // We do all this to allow implicit conversion of the output to List<DataType>
+                Type genericListType = typeof(List<>).MakeGenericType(this.dataType);
+                IList retObjs = (IList)Activator.CreateInstance(genericListType);
+
+                foreach (object obj in objs) {
+                    retObjs.Add(obj);
+                }
+
+                yield return new object[] { retObjs };
+            }
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data) {
+            if (data == null || data.Length == 0)
+                return null;
+
+            return string.Format(
+                CultureInfo.CurrentCulture,
+                "{0} - ({1})",
+                methodInfo.Name,
+                TestHelpers.GetObjectStr(data[0])
+            );
+        }
+    }
+}
---
+++ KpcUwpCore/Testing/TestHelpers.cs
@@ -0,0 +1,50 @@
+﻿/**
+ * TestHelpers.cs
+ *
+ * Copyright (c) 2021 Kano Computing Ltd.
+ * License: https://opensource.org/licenses/MIT
+ */
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace KanoComputing.KpcUwpCore.Testing {
+
+    public class TestHelpers {
+
+        public static IEnumerable<IEnumerable<T>> GetPowerSet<T>(IEnumerable<T> input) {
+            var seed = new List<IEnumerable<T>>() { Enumerable.Empty<T>() }
+              as IEnumerable<IEnumerable<T>>;
+
+            return input.Aggregate(seed, (a, b) =>
+              a.Concat(a.Select(x => x.Concat(new List<T>() { b }))));
+        }
+
+        public static string GetObjectStr(object obj) {
+            if (obj is IEnumerable<object>) {
+                IEnumerable<object> listObj = obj as IEnumerable<object>;
+                return String.Join(",", listObj);
+            }
+            return obj.ToString();
+        }
+
+        public static IEnumerable<T> UnpackFixtureData<T>(ITestDataSource fixture, Action method) where T : class {
+            return fixture.GetData(method.Method)
+                .Select(x => x[0] as T);
+        }
+
+        public static void AssertJObjectContains<T>(JObject obj, string key, T prop, bool isRequired = false) {
+            if (!obj.ContainsKey(key)) {
+                Assert.IsFalse(isRequired, $"The key {key} is required but not present in the JObject");
+                Assert.AreEqual(default, prop, $"{key} was not in the JObject but wasn't default in the object");
+                return;
+            }
+            Assert.AreEqual(obj[key].ToObject<T>(), prop, $"{key} didn't match");
+        }
+    }
+}
