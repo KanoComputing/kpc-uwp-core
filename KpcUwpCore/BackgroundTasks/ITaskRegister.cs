65c0a8cc3cb244f3d058eab2abb3e0bf5d8ca746
radujipa <radu.t.jipa@gmail.com>
2021-07-08 19:09:57 +0100

    Background task registration helper

---
+++ KpcUwpCore/BackgroundTasks/ITaskRegister.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * ITaskRegister.cs
+ *
+ * Copyright (c) 2021 Kano Computing Ltd.
+ * License: https://opensource.org/licenses/MIT
+ */
+
+
+using System.Collections.Generic;
+using Windows.ApplicationModel.Background;
+
+
+namespace KanoComputing.KpcUwpCore.BackgroundTasks {
+
+    public interface ITaskRegister {
+
+        IBackgroundTaskRegistration RegisterBackgroundTask<T>(
+            IBackgroundTrigger trigger,
+            IReadOnlyList<IBackgroundCondition> conditions = null);
+    }
+}
